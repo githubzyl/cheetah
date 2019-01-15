08bf1912d28c0423278bb9c1ab9b9c1e50bfbd83
githubzyl <517190473@qq.com>
2019-01-15 10:53:52 +0800

    修改用户信息绑定表

--- cheetah-web-backstage/src/test/java/top/zylsite/cheetah/web/backstage/Test1.java
+++ cheetah-web-backstage/src/test/java/top/zylsite/cheetah/web/backstage/Test1.java
@@ -106,5 +106,39 @@ public class Test1 {
 		System.out.println("总数：" + accounts.size());
 		LDAPUtil.close(ctx);
 	}
+	
+	@Test
+	public void testIpToLong() {
+		String ip = "192.168.12.36";
+		long longIp = ipToLong(ip);
+		System.out.println(numberToIp(longIp).equals(ip));
+	}
+	
+	private static String numberToIp(Long number) {
+        //等价上面
+        String ip = "";
+        for (int i = 3; i >= 0; i--) {
+            ip += String.valueOf((number & 0xff));
+            if (i != 0) {
+                ip += ".";
+            }
+            number = number >> 8;
+        }
+ 
+        return ip;
+    }
+
+	public static long ipToLong(String ip) {
+        String[] ipArray = ip.split("\\.");
+        List ipNums = new ArrayList();
+        for (int i = 0; i < 4; ++i) {
+            ipNums.add(Long.valueOf(Long.parseLong(ipArray[i].trim())));
+        }
+        long ZhongIPNumTotal = ((Long) ipNums.get(0)).longValue() * 256L * 256L * 256L
+                + ((Long) ipNums.get(1)).longValue() * 256L * 256L + ((Long) ipNums.get(2)).longValue() * 256L
+                + ((Long) ipNums.get(3)).longValue();
+ 
+        return ZhongIPNumTotal;
+    }
 
 }
